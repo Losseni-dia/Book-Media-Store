c32cd78886c76f2a4296e1407e63eb9dab6f26d0
Losseni Dia <losdiakite@gmail.com>
2026-02-16 20:21:58 +0100

    hello

---
+++ src/main/java/icc/web/book_media_store/BookMediaStoreApplication.java
@@ -0,0 +1,13 @@
+package icc.web.book_media_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMediaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMediaStoreApplication.class, args);
+	}
+
+}
